236a9aa9a67fda29c0b117a337fa39a86413b4ab
Luis Ernesto Diaz Cantillo <lurdecan1094@hotmail.com>
2025-12-05 11:10:21 -0500

    0008 add select with filters and modified how to show data

--- frontend/src/components/AsociadosList.tsx
+++ frontend/src/components/AsociadosList.tsx
@@ -1,37 +1,55 @@
 import { useAsociados } from '../hooks/useAsociados';
-
+import SelectFiltro from './SelectFiltro';
+import React, { useState } from 'react';
 const AsociadosList = () => {
 
   const { asociados, loading, error } = useAsociados();  
-  
-  if (loading) return <p>Cargando Datos...</p>;
-  if (error) return <p>Error: {error} </p> 
+  const [filtro, setFiltro] = useState("Todos");
 
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    console.log(e.target.value);
+    setFiltro(e.target.value);
+  };
+   // FILTRAR ASOCIADOS SEGÚN EL SELECT
+  const asociadosFiltrados =
+    filtro === "Todos"
+      ? asociados
+      : asociados.filter((a) => a.estado_pipeline === filtro);
+  
+      if (loading) return <p>Cargando Datos...</p>;
+      if (error) return <p>Error: {error} </p> 
 
   return (
     <div className='Container'>
         <h2>Listado de Asociados</h2>
 
-        <table border={1} cellPadding={8} style={{ width: "100%", borderCollapse: "collapse" }}>
-            <thead>
-                <th>Nombre</th>
-                <th>Identificación</th>
-                <th>Estado Pipeline</th>
-            </thead>
-
-            <tbody>
-                {asociados.map((asociado) => {
-                    return (
-                        <tr key ={asociado.id}>
-                        <td>{asociado.Nombre}</td>
-                        <td>{asociado.Identificación}</td>
-                        <td>{asociado.estado_pipeline}</td>
-                    </tr>
-                    );   
-                })}
-            </tbody>
-        </table>
-    </div>
+       <SelectFiltro value={filtro} handleSelectChange={handleSelectChange} />
+        {asociadosFiltrados.length === 0 ? (
+            <p>No hay asociados disponibles</p>
+        ): (
+            <table border={1} cellPadding={8} style={{ width: "100%", borderCollapse: "collapse" }}>
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Identificación</th>
+                                <th>Estado Pipeline</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {asociadosFiltrados.map((asociado) => {
+                                return (
+                                    <tr key ={asociado.id}>
+                                    <td>{asociado.Nombre}</td>
+                                    <td>{asociado.Identificación}</td>
+                                    <td>{asociado.estado_pipeline}</td>
+                                </tr>
+                                );   
+                            })}
+                        </tbody>
+                    </table>
+                    )}    
+                    
+         </div>
   )
 }
 
